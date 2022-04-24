111eecf0b20304c1d71878f9c3f9cac634a4963a
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-04-24 12:22:10 -0300

    Capitalizing the title

--- modelo.py
+++ modelo.py
@@ -1,12 +1,12 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
